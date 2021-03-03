0a9076b31f86c6862e12e95ed5b213badd6d36b3
ayoub.benkhemis <ayoub.benkhemis@gmail.com>
2021-03-03 18:44:38 +0100

    provide GetVideos use case with Hilt

---
+++ app/src/main/java/com/bks/eurosporttest/di/InteractorsModule.kt
@@ -0,0 +1,29 @@
+package com.bks.eurosporttest.di
+
+import com.bks.eurosporttest.data.network.ApiService
+import com.bks.eurosporttest.data.network.mapper.VideoDtoMapper
+import com.bks.eurosporttest.interactors.featured.GetVideos
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object InteractorsModule {
+
+    @ViewModelScoped
+    @Provides
+    fun provideGetVideos(
+        apiService: ApiService,
+        videoDtoMapper: VideoDtoMapper
+    ):GetVideos {
+        return GetVideos(
+            apiService,
+            videoDtoMapper
+        )
+    }
+
+}
